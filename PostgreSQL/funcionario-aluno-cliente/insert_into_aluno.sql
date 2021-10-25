173837420f1f8e0a39ce941ea50da14c94d761a7
pedrovbo <pedrovicboliveira@gmail.com>
2021-10-25 18:08:37 -0300

    add some old PostgreSQL studies

---
+++ PostgreSQL/funcionario-aluno-cliente/insert_into_aluno.sql
@@ -0,0 +1,24 @@
+INSERT INTO ALUNO (CODIGOALUNO, NOME, CPF, SEXO, DTNASCIMENTO)
+VALUES (
+        1,
+        'JOSÉ FRANCISCO TERRA',
+        '82988',
+        'M',
+        '28/10/1989'
+    ),
+    (2, 'ANDREY COSTA FILHO', '0024', 'M', '20/10/1999'),
+    (
+        3,
+        'ROBERTA SILVA BRASIL',
+        '82998',
+        'F',
+        '20/02/1980'
+    ),
+    (4, 'CARLA MARIA MACIEL', '0044', 'F', '20/11/1996'),
+    (
+        5,
+        'MARCOS PEREIRA BRASIL',
+        '9999',
+        'M',
+        '20/02/1999'
+    );
---
+++ PostgreSQL/funcionario-aluno-cliente/insert_into_funcionario.sql
@@ -0,0 +1,132 @@
+INSERT INTO FUNCIONARIO (
+        CODIGOFUNCIONARIO,
+        NOME,
+        CPF,
+        SEXO,
+        DTNASCIMENTO,
+        SALARIO
+    )
+VALUES (
+        1,
+        'ROBERTA SILVA BRASIL',
+        '82998',
+        'F',
+        '20/02/1980',
+        7000
+    );
+INSERT INTO FUNCIONARIO (
+        CODIGOFUNCIONARIO,
+        NOME,
+        CPF,
+        SEXO,
+        DTNASCIMENTO,
+        SALARIO
+    )
+VALUES (
+        2,
+        'MARIA SILVA BRASIL',
+        '9876',
+        'F',
+        '20/09/1988',
+        9500
+    );
+INSERT INTO FUNCIONARIO (
+        CODIGOFUNCIONARIO,
+        NOME,
+        CPF,
+        SEXO,
+        DTNASCIMENTO,
+        SALARIO
+    )
+VALUES (
+        3,
+        'GABRIELLA PEREIRA
+LIMA',
+        '32998',
+        'F',
+        '20/02/1990',
+        6000
+    );
+INSERT INTO FUNCIONARIO (
+        CODIGOFUNCIONARIO,
+        NOME,
+        CPF,
+        SEXO,
+        DTNASCIMENTO,
+        SALARIO
+    )
+VALUES (
+        4,
+        'MARCOS PEREIRA BRASIL',
+        '9999',
+        'M',
+        '20/02/1999',
+        6000
+    );
+INSERT INTO FUNCIONARIO (
+        CODIGOFUNCIONARIO,
+        NOME,
+        CPF,
+        SEXO,
+        DTNASCIMENTO,
+        SALARIO
+    )
+VALUES (
+        5,
+        'HEMERSON SILVA BRASIL',
+        '9111',
+        'M',
+        '20/12/1992',
+        4000
+    );
+/*or*/
+INSERT INTO FUNCIONARIO (
+        CODIGOFUNCIONARIO,
+        NOME,
+        CPF,
+        SEXO,
+        DTNASCIMENTO,
+        SALARIO
+    )
+VALUES (
+        1,
+        'ROBERTA SILVA BRASIL',
+        '82998',
+        'F',
+        '20/02/1980',
+        7000
+    ),
+,
+(
+        2,
+        'MARIA SILVA BRASIL',
+        '9876',
+        'F',
+        '20/09/1988',
+        9500
+    ),
+(
+        3,
+        'GABRIELLA PEREIRA
+LIMA',
+        '32998',
+        'F',
+        '20/02/1990',
+        6000
+    ),
+(
+        4,
+        'MARCOS PEREIRA BRASIL',
+        '9999',
+        'M',
+        '20/02/1999',
+        6000
+    ),
+(
+        5,
+        'HEMERSON SILVA BRASIL',
+        '9111',
+        'M',
+        '20/12/1992',
+        4000
+    );
